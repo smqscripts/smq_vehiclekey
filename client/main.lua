0b9074e2d7883893900deaae7809d0b7154b1ce7
SMQ SCRIPTS <84682514+smqscripts@users.noreply.github.com>
2026-02-07 00:32:46 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -4,55 +4,51 @@ local function handleToggle()
     if isBusy then return end
     
     local ped = PlayerPedId()
-    local pos = GetEntityCoords(ped)
-    local vehicle = lib.getClosestVehicle(pos, 4.0, false)
+    local coords = GetEntityCoords(ped)
+    local vehicle = lib.getClosestVehicle(coords, 4.0, false)
 
     if not vehicle or vehicle == 0 then return end
     
-    local vPos = GetEntityCoords(vehicle)
-    if #(pos - vPos) > 5.0 then return end
-
     local plate = GetVehicleNumberPlateText(vehicle):gsub("%s+", "")
-    local hasKey = exports.ox_inventory:Search('count', 'vehicle_key', {plate = plate})
+    local netId = NetworkGetNetworkIdFromEntity(vehicle)
 
-    if hasKey and hasKey > 0 then
-        isBusy = true
-        lib.requestAnimDict('anim@mp_player_intmenu@key_fob@')
-        TaskPlayAnim(ped, 'anim@mp_player_intmenu@key_fob@', 'fob_click', 8.0, 8.0, -1, 48, 1, false, false, false)
-        
-        local lockStatus = GetVehicleDoorLockStatus(vehicle)
-        local newState = lockStatus > 1 and 1 or 2
-        
-        if not NetworkHasControlOfEntity(vehicle) then
-            NetworkRequestControlOfEntity(vehicle)
-        end
+    isBusy = true
+    
+    lib.requestAnimDict('anim@mp_player_intmenu@key_fob@')
+    TaskPlayAnim(ped, 'anim@mp_player_intmenu@key_fob@', 'fob_click', 8.0, 8.0, -1, 48, 1, false, false, false)
+    
+    TriggerServerEvent('smg_key:server:toggleLock', netId, plate)
 
-        SetVehicleDoorsLocked(vehicle, newState)
-        SetVehicleLights(vehicle, 2)
-        PlayVehicleDoorOpenSound(vehicle, 0)
-        Wait(200)
-        SetVehicleLights(vehicle, 0)
-        
-        lib.notify({
-            title = 'Vehicle',
-            description = newState == 2 and 'Vehicle Locked' or 'Vehicle Unlocked',
-            type = newState == 2 and 'inform' or 'success'
-        })
-        
-        SetTimeout(600, function() isBusy = false end)
-    else
-        lib.notify({ description = 'No keys for '..plate, type = 'error' })
-    end
+    SetTimeout(800, function() isBusy = false end)
 end
 
 lib.addKeybind({
     name = 'veh_lock_sys',
-    description = 'Lock/Unlock',
+    description = 'Lock/Unlock Vehicle',
     defaultKey = 'L',
     onPressed = function()
         handleToggle()
     end
 })
+
+RegisterNetEvent('smg_key:client:syncEffects', function(netId)
+    local vehicle = NetToVeh(netId)
+    if DoesEntityExist(vehicle) then
+        SetVehicleLights(vehicle, 2)
+        PlayVehicleDoorOpenSound(vehicle, 0)
+        SetTimeout(200, function()
+            SetVehicleLights(vehicle, 0)
+        end)
+    end
+end)
+
+AddStateBagChangeHandler('lockStatus', nil, function(bagName, key, value, _unused, replicated)
+    local netId = tonumber(bagName:gsub('entity:', ''))
+    local entity = NetToVeh(netId)
+    if DoesEntityExist(entity) then
+        SetVehicleDoorsLocked(entity, value)
+    end
+end)
 
 RegisterNetEvent('smg_key:client:reqKey', function()
     local ped = PlayerPedId()
