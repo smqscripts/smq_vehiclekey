4b502f5d6a56b9725d86b2e9c6b7eb722fa12898
SMQ SCRIPTS <84682514+smqscripts@users.noreply.github.com>
2026-02-06 13:53:33 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,64 +1,70 @@
-local lang = {
-    title = 'Vozidlo',
-    locked = 'Zamknuto',
-    unlocked = 'Odemknuto',
-    no_key = 'Nemáš klíče od tohoto vozidla.',
-    bind = 'Zamknout/Odemknout vozidlo'
-}
+local isBusy = false
 
-CreateThread(function()
-    lib.requestAnimDict('anim@mp_player_intmenu@key_fob@')
-end)
-
-local function toggleVehicleState(veh)
-    local plate = GetVehicleNumberPlateText(veh):gsub("^%s*(.-)%s*$", "%1")
-    local hasItem = exports.ox_inventory:Search('count', 'vehicle_key', {plate = plate})
+local function handleToggle()
+    if isBusy then return end
     
-    if (hasItem or 0) > 0 then
-        TaskPlayAnim(PlayerPedId(), 'anim@mp_player_intmenu@key_fob@', 'fob_click', 8.0, 8.0, -1, 48, 1, false, false, false)
+    local ped = PlayerPedId()
+    local pos = GetEntityCoords(ped)
+    local vehicle = lib.getClosestVehicle(pos, 4.0, false)
+
+    if not vehicle or vehicle == 0 then return end
+    
+    local vPos = GetEntityCoords(vehicle)
+    if #(pos - vPos) > 5.0 then return end
+
+    local plate = GetVehicleNumberPlateText(vehicle):gsub("%s+", "")
+    local hasKey = exports.ox_inventory:Search('count', 'vehicle_key', {plate = plate})
+
+    if hasKey and hasKey > 0 then
+        isBusy = true
+        lib.requestAnimDict('anim@mp_player_intmenu@key_fob@')
+        TaskPlayAnim(ped, 'anim@mp_player_intmenu@key_fob@', 'fob_click', 8.0, 8.0, -1, 48, 1, false, false, false)
         
-        local isLocked = GetVehicleDoorLockStatus(veh) > 1
-        local newState = isLocked and 1 or 2
+        local lockStatus = GetVehicleDoorLockStatus(vehicle)
+        local newState = lockStatus > 1 and 1 or 2
         
-        SetVehicleDoorsLocked(veh, newState)
-        SetVehicleLights(veh, 2)
+        if not NetworkHasControlOfEntity(vehicle) then
+            NetworkRequestControlOfEntity(vehicle)
+        end
+
+        SetVehicleDoorsLocked(vehicle, newState)
+        SetVehicleLights(vehicle, 2)
+        PlayVehicleDoorOpenSound(vehicle, 0)
         Wait(200)
-        SetVehicleLights(veh, 0)
+        SetVehicleLights(vehicle, 0)
         
         lib.notify({
-            title = lang.title,
-            description = isLocked and lang.unlocked or lang.locked,
-            type = isLocked and 'success' or 'inform'
+            title = 'Vehicle',
+            description = newState == 2 and 'Vehicle Locked' or 'Vehicle Unlocked',
+            type = newState == 2 and 'inform' or 'success'
         })
+        
+        SetTimeout(600, function() isBusy = false end)
     else
-        lib.notify({ description = lang.no_key, type = 'error' })
+        lib.notify({ description = 'No keys for '..plate, type = 'error' })
     end
 end
 
 lib.addKeybind({
-    name = 'veh_lock_system',
-    description = lang.bind,
+    name = 'veh_lock_sys',
+    description = 'Lock/Unlock',
     defaultKey = 'L',
     onPressed = function()
-        local veh = lib.getClosestVehicle(GetEntityCoords(PlayerPedId()), 5.0, false)
-        if veh then toggleVehicleState(veh) end
+        handleToggle()
     end
 })
 
-RegisterNetEvent('smg_key:client:requestNearbyKey', function()
-    local veh = lib.getClosestVehicle(GetEntityCoords(PlayerPedId()), 5.0, false)
-    if veh then
-        TriggerServerEvent('smg_key:server:requestKey', GetVehicleNumberPlateText(veh))
-    end
+RegisterNetEvent('smg_key:client:reqKey', function()
+    local ped = PlayerPedId()
+    local v = lib.getClosestVehicle(GetEntityCoords(ped), 3.5, false)
+    if v then TriggerServerEvent('smg_key:server:giveKey', GetVehicleNumberPlateText(v)) end
 end)
 
 RegisterNetEvent('cd_garage:AddKeys', function(plate)
-    if plate then TriggerServerEvent('smg_key:server:requestKey', plate) end
+    if plate then TriggerServerEvent('smg_key:server:giveKey', plate) end
 end)
 
 RegisterNetEvent('cd_garage:StoreVehicle', function()
-    local pedVeh = GetVehiclePedIsIn(PlayerPedId(), true)
-    if pedVeh ~= 0 then
-        TriggerServerEvent('smg_key:server:removeKey', GetVehicleNumberPlateText(pedVeh))
-    end
+    local v = GetVehiclePedIsIn(PlayerPedId(), true)
+    if v ~= 0 then TriggerServerEvent('smg_key:server:remKey', GetVehicleNumberPlateText(v)) end
 end)
